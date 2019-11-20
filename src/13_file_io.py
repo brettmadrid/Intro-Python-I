11a16341252686db5cb1acea232fd5d4b5196196
brettmadrid <brettmadrid9@gmail.com>
2019-11-19 16:50:52 -0800

    Completed file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,7 +8,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
+
 # YOUR CODE HERE
+def load_file():
+    text_file = open('foo.txt', 'r')
+    print(text_file.read())
+    text_file.close()
+
+
+load_file()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +24,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+def write_file():
+    text_file = open('bar.txt', 'w+')
+    text_file.write('sayaret matkal\r\n' + 'givurati brigade\r\n' + 'Adonai Echad!\r\n')
+    text_file.close()
+
+
+write_file()
